66b27213e8f00747163f00fee135ef37a0e09c74
Mosh <moshmosh.ca@gmail.com>
2025-04-30 14:21:43 +0200

    Add variables for HTML elements in JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get reference to DOM elements
+const form = document.getElementById("grocery-form");
+const itemNameInput = document.getElementById("item-name");
+const itemQtyInput = document.getElementById("item-qty");
+const groceryList = document.getElementById("grocery-list");
