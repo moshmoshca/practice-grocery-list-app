91d637dee61d6280abcdf7ade1f6399515734dcf
Mosh <moshmosh.ca@gmail.com>
2025-04-30 14:33:07 +0200

    Add event listeners to the form

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const form = document.getElementById("grocery-form");
 const itemNameInput = document.getElementById("item-name");
 const itemQtyInput = document.getElementById("item-qty");
 const groceryList = document.getElementById("grocery-list");
+
+// Add event listeners to the form
+form.addEventListener("submit", function (e){
+    e.preventDefault(); // Stop the page from refreshing
+
+    const name = itemNameInput.value.trim(); // Get the name value
+    const qty = itemQtyInput.value.trim(); // Get the quantity value
+
+    if (name === "" || qty === "") return; // Avoid adding empty items
+
+});
